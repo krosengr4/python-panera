861a233708f67a3d26e4f0526fd212ddcc422806
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-13 06:00:48 -0500

    Created a class for sandwiches

--- python-panera/menu_item.py
+++ python-panera/menu_item.py
@@ -1,3 +1,17 @@
 class MenuItem:
     def __init__(self, name):
         self.name = name
+
+class Sandwich(MenuItem):
+    def __init__(self, name, size, bread, meat, cheese, is_toasted):
+        super().__init__(name)
+        self.size = size
+        self.bread = bread
+        self.meat = meat 
+        self.cheese = cheese 
+        self.is_toasted = is_toasted
+        self.toppings = []
+        self.sauces = [] 
+
+    def calculate_price():
+        if
