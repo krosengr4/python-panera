c1829800bfb886655aa3aab2ddf9a94a7dc7cb7c
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-13 03:42:50 -0500

    Created screen for ordering in ui

--- python-panera/user_interface.py
+++ python-panera/user_interface.py
@@ -1,4 +1,4 @@
-def display_main_menu():
+def main_menu():
     print('\n\t-----MAIN MENU------')
     print("""
             ---OPTIONS---
@@ -10,3 +10,16 @@ def display_main_menu():
     
     return int(input('Enter your option:\n'))
 
+def order_screen():
+    print('\n\t-----ORDER------')
+    print("""
+            ---OPTIONS---
+        1 - Order Sandwich
+        2 - Order Chips
+        3 - Order Drink
+        4 - Checkout
+        
+        0 - Cancel
+          """)
+    
+    return int(input('Enter your option:\n'))
