4de16dedcb21e99362f289463da17f7ae1f79bba
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-13 14:26:33 -0500

    Created the skeleton for the sandwich logic

--- python-panera/sandwich_logic.py
+++ python-panera/sandwich_logic.py
@@ -3,5 +3,23 @@ import user_interface
 def process_make_sandwich(order):
     print('Make a new sandwich')
 
+def process_size():
+    print('Size')
+
 def process_bread_choice():
     user_interface.bread_options()
+
+def process_meat():
+    print('Meat')
+
+def process_cheese():
+    print('Cheese')
+
+def process_toppings():
+    print('Toppings')
+
+def process_sauces():
+    print('Sauces')
+
+def process_toasted():
+    print('Toasted or not')
