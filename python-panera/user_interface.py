9c20a48637cbb4f678d5b09ec9180c9256a125ab
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-13 03:56:23 -0500

    Created methods for cheese toppings and sauce menus

--- python-panera/user_interface.py
+++ python-panera/user_interface.py
@@ -59,3 +59,41 @@ def meat_options():
           """)
     
     return int(input('Enter your option:\n'))
+
+def cheese_options():
+    print('\n\t-----CHOOSE YOUR CHEESE-----')
+    print("""
+            ---OPTIONS---
+        1 - American
+        2 - Provolone
+        3 - Cheddar
+        4 - Swiss
+        5 - No cheese
+          """)
+    
+    return int(input('Enter your option:\n'))
+
+def toppings_options():
+    print('\n\t-----CHOOSE YOUR TOPPINGS-----')
+    print("""
+                    ---OPTIONS---
+            1 - Lettuce         6 - Cucumbers             
+            2 - Peppers         7 - Pickles
+            3 - Onions          8 - Guacamole
+            4 - Tomatoes        9 - Mushrooms
+            5 - Jalepenos     
+          """)
+    
+    return int(input('Enter your option:\n'))
+
+def sauce_options():
+    print('\n\t-----CHOOSE YOUR SAUCE-----')
+    print("""
+                        ---OPTIONS---
+            1 - Mayo                5 - Vinnigrette                              
+            2 - Mustard             6 - Au Jus
+            3 - Ketchup             7 - Barbeque 
+            4 - Thousand Islands    8 - Relish 
+          """)
+    
+    return int(input('Enter your option:\n'))
