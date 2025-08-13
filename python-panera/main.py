98dd83227106a1030688a9553f2af527d33a47c2
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-13 03:36:49 -0500

    Set up main file for project

---
+++ python-panera/main.py
@@ -0,0 +1,11 @@
+import main_logic
+
+def main():
+    print('\n----------WELCOME TO PYTHON PANERA!----------')
+    main_logic.process_main_menu()
+
+    print('\n\nGoodbye!!!')
+    print('______________________________________________')
+    print('See you soon!!!')
+
+main()
---
+++ python-panera/main_logic.py
@@ -0,0 +1,4 @@
+import user_interface
+
+def process_main_menu():
+    print('Main menu')
