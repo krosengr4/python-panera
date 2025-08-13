eb2e16601fe242edb17292988d83784e5605ad8b
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-13 07:19:39 -0500

    Created logic method for user to select their drink choice

--- python-panera/checkout_logic.py
+++ python-panera/checkout_logic.py
@@ -4,3 +4,5 @@ def process_checkout(order):
 
     order.calculate_total()
     order.print_order()
+
+    input('Press enter to continue')
--- python-panera/drink_logic.py
+++ python-panera/drink_logic.py
@@ -2,3 +2,31 @@ import user_interface
 
 def process_add_drink(order):
     print('drink')
+
+def drink_type():
+
+    if_continue = True
+
+    while(if_continue):
+        user_choice = user_interface.drink_opitons()
+
+        match user_choice:
+            case 1:
+                drink_choice = 'Fountain Drink'
+                if_continue = False
+            case 2:
+                drink_choice = 'Lemonade'
+                if_continue = False
+            case 3:
+                drink_choice = ' Milkshake'
+                if_continue = False
+            case 4:
+                drink_choice = 'Sweet Tea'
+                if_continue = False
+            case 5:
+                drink_choice = 'Unsweet Tea'
+                if_continue = False
+            case _:
+                print('Please enter a valid option!')
+
+    return drink_choice
