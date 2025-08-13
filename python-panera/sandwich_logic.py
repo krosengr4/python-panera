e227711b232419136cbbf8b0594cf1e600e01c52
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-13 16:39:38 -0500

    Created logic method for user to choose the size of their sandwich

--- python-panera/sandwich_logic.py
+++ python-panera/sandwich_logic.py
@@ -1,10 +1,28 @@
 import user_interface
+from menu_item import Sandwich
+from order import Order
 
 def process_make_sandwich(order):
     print('Make a new sandwich')
 
 def process_size():
-    print('Size')
+    if_continue = True
+
+    while if_continue:
+        size_choice = user_interface.sandwich_sizes()
+        match size_choice:
+            case 1:
+                size = 'small'
+                if_continue = False
+            case 2:
+                size = 'medium'
+                if_continue = False
+            case 3:
+                size = 'large'
+                if_continue = False
+            case _:
+                print('Please enter a valid option!!!')
+
 
 def process_bread_choice():
     user_interface.bread_options()
